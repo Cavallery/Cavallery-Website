b9731e9e41ea160a5f0a51dd308c113af4b35225
Valzz <jkt48connect@gmail.com>
2026-05-27 10:05:43 +0700

    Update AboutErineSection.tsx

--- src/components/home/AboutErineSection.tsx
+++ src/components/home/AboutErineSection.tsx
@@ -256,24 +256,24 @@ export default function AboutErineSection() {
           <div className={styles.socialSection} id="sosmed">
             <div className={styles.socialTitle}>Erine's Social Media</div>
             <div className={styles.socialIcons}>
-              <a href="https://x.com/CErine_JKT48" target="_blank" className={styles.socIcon}>
+              <a href="https://x.com/CErine_JKT48" target="_blank" rel="noopener noreferrer" className={styles.socIcon}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
                 </svg>
               </a>
-              <a href="https://www.instagram.com/jkt48.erine/" target="_blank" className={styles.socIcon}>
+              <a href="https://www.instagram.com/jkt48.erine/" target="_blank" rel="noopener noreferrer" className={styles.socIcon}>
                 <i className="bx bxl-instagram" />
               </a>
-              <a href="https://www.threads.com/@jkt48.erine" target="_blank" className={styles.socIcon}>
+              <a href="https://www.threads.com/@jkt48.erine" target="_blank" rel="noopener noreferrer" className={styles.socIcon}>
                 <i className="bx bxl-facebook-circle" />
               </a>
-              <a href="https://www.tiktok.com/@jkt48.erine_" target="_blank" className={styles.socIcon}>
+              <a href="https://www.tiktok.com/@jkt48.erine_" target="_blank" rel="noopener noreferrer" className={styles.socIcon}>
                 <i className="bx bxl-tiktok" />
               </a>
-              <a href="https://www.showroom-live.com/r/JKT48_Erine" target="_blank" className={styles.socIcon}>
+              <a href="https://www.showroom-live.com/r/JKT48_Erine" target="_blank" rel="noopener noreferrer" className={styles.socIcon}>
                 <img src="/images/showroom.png" alt="Showroom" style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "50%" }} />
               </a>
-              <a href="https://www.idn.app/jkt48_erine" target="_blank" className={styles.socIcon}>
+              <a href="https://www.idn.app/jkt48_erine" target="_blank" rel="noopener noreferrer" className={styles.socIcon}>
                 <img src="/images/idn.png" alt="IDN" style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "50%" }} />
               </a>
             </div>
