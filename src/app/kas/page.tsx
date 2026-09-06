283e5021f9ae1d502669e346a0c9fa79322a8a41
adityaptm <putraadityapratama236@gmail.com>
2026-09-06 13:23:01 +0700

    FIX BAYAR KAS V1

--- src/app/kas/page.tsx
+++ src/app/kas/page.tsx
@@ -629,7 +629,7 @@ export default function KasPage() {
               </button>
             </form>
           </div>
-        )}
+        ))}
 
         {/* Tab 2: Cek Status Kas History */}
         {activeTab === "status" && (
